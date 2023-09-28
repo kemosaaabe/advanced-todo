ade969d6f8d90d7c3ff354f54b4e560b28d1c90f
kemosaaabe <sanchellos27@mail.ru>
2023-09-28 18:42:03 +0300

    save redux store to local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.css";
 import App from "./App";
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
