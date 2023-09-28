6f5c71a2495ec649637e4e4f1f94b56656ed1ff2
kemosaaabe <sanchellos27@mail.ru>
2023-09-28 11:51:57 +0300

    created ui modal

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,13 +1,24 @@
+import React from "react";
+
 import Project from "../../components/Project";
 import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
 
 import styles from "./styles.module.css";
 
 const Projects = () => {
+  const [visibleModal, setVisibleModal] = React.useState(false);
+
+  const onCloseModal = () => {
+    setVisibleModal(!visibleModal);
+  };
+
   return (
     <div className={styles.container}>
+      {visibleModal && <Modal onClose={onCloseModal}>Добавить проект</Modal>}
+
       <h1 className={styles.title}>Проекты</h1>
-      <Button>Создать проект</Button>
+      <Button onClick={onCloseModal}>Создать проект</Button>
       <div className={styles.projects}>
         {Array(10)
           .fill(null)
--- src/ui/Modal/index.tsx
+++ src/ui/Modal/index.tsx
@@ -1,5 +1,40 @@
-const Modal = () => {
-  return <div></div>;
+import React, { FC } from "react";
+import { createPortal } from "react-dom";
+
+import styles from "./styles.module.css";
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const Modal: FC<ModalProps> = ({ onClose, children }) => {
+  React.useLayoutEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  });
+
+  const ModalComponent = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.cover} onClick={onClose}></div>
+        <div className={styles.modal}>
+          <img
+            onClick={onClose}
+            className={styles.closeIcon}
+            src="/assets/images/close.svg"
+            alt="close"
+          />
+          {children}
+        </div>
+      </div>
+    );
+  };
+
+  return createPortal(<ModalComponent />, document.body);
 };
 
 export default Modal;
