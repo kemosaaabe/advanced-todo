94bff9d090401e10981830c68d5702d1a1cde7e7
kemosaaabe <sanchellos27@mail.ru>
2023-09-28 09:58:23 +0300

    created files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+
 function App() {
-  return <div>Санек</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Projects />} />
+      <Route path="/projects/:projectId" element={<Tasks />} />
+    </Routes>
+  );
 }
 
 export default App;
