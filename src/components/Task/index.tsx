f4a79f515f2d0ebdc06cb6f1445a313b85223dba
kemosaaabe <sanchellos27@mail.ru>
2023-09-30 17:40:15 +0300

    simple layout for tasks

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,5 +1,7 @@
+import styles from "./styles.module.css";
+
 const Task = () => {
-  return <div></div>;
+  return <div className={styles.task}>Task</div>;
 };
 
 export default Task;
--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,5 +1,64 @@
+import { useParams, Link } from "react-router-dom";
+
+import { useAppSelector } from "../../app/hooks";
+
+import Button from "../../ui/Button";
+import Task from "../../components/Task";
+
+import styles from "./styles.module.css";
+
 const Tasks = () => {
-  return <div>task</div>;
+  const { projectId } = useParams();
+
+  const project = useAppSelector((state) =>
+    state.projects.projects.find((project) => project.id === projectId)
+  );
+
+  return (
+    <div className={styles.container}>
+      <Link to="/">
+        <img
+          className={styles.arrow}
+          src="/assets/images/arrow.svg"
+          alt="arrow back"
+        />
+      </Link>
+      <h1 className={styles.title}>Задачи</h1>
+      <Button>Создать задачу</Button>
+      <div className={styles.tasks}>
+        <div className={styles.col}>
+          <h2 className={styles.titleCol}>Queue</h2>
+          <div className={styles.colInner}>
+            {Array(10)
+              .fill(null)
+              .map(() => (
+                <Task />
+              ))}
+          </div>
+        </div>
+        <div className={styles.col}>
+          <h2 className={styles.titleCol}>Development</h2>
+          <div className={styles.colInner}>
+            {Array(10)
+              .fill(null)
+              .map(() => (
+                <Task />
+              ))}
+          </div>
+        </div>
+        <div className={styles.col}>
+          <h2 className={styles.titleCol}>Done</h2>
+          <div className={styles.colInner}>
+            {Array(10)
+              .fill(null)
+              .map(() => (
+                <Task />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
